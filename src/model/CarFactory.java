5659f3d2bff2cc2804f8228f139dfe90f228dc4c
Dörrich <hugo.drrich@yahoo.com>
2025-03-04 01:57:29 +0100

    Update CarFactory.java

--- src/model/CarFactory.java
+++ src/model/CarFactory.java
@@ -1,14 +1,23 @@
 package model;
 
 import model.exceptions.CarNotFound;
+import java.util.Random;
 
 public class CarFactory {
-    public static Car createCar(String carType) { // maybe use enum?
+    public enum CarType {
+        Saab95, Volvo240, Scania;
+
+        public static CarType getRandomType() {
+            CarType[] values = CarType.values();
+            return values[new Random().nextInt(values.length)];
+        }
+    }
+
+    public static Car createCar(CarType carType) {
         return switch (carType) {
-            case "Saab95" -> new Saab95();
-            case "Volvo240" -> new Volvo240();
-            case "Scania" -> new Scania();
-            default -> throw new CarNotFound("Unknown car type: " + carType);
+            case Saab95 -> new Saab95();
+            case Volvo240 -> new Volvo240();
+            case Scania -> new Scania();
         };
     }
 }
